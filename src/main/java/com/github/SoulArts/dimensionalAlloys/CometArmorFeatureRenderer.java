b15debf3530e59cf803c6dee09269dcc8f998395
Soulphur <ismaelbe2002@gmail.com>
2022-11-06 01:30:33 +0100

    Tried to make CometArmorFeatureRenderer exactly like the vanilla one, but the comet armor model nor extends from BipedEntityModel or is intended to behave like regular armor.

--- src/main/java/com/github/SoulArts/dimensionalAlloys/CometArmorFeatureRenderer.java
+++ src/main/java/com/github/SoulArts/dimensionalAlloys/CometArmorFeatureRenderer.java
@@ -9,53 +9,90 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.AnimalModel;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.DyeableArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
-public class CometArmorFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends EntityModel<T>> extends FeatureRenderer<T, M>  {
-    private static final Identifier ENDBRITE_HELMET_TEXTURE = new Identifier("textures/endbrite_helmet.png");
-    private static final Identifier ENDBRITE_CHESTPLATE_TEXTURE = new Identifier("textures/endbrite_chestplate.png");
+public class CometArmorFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M>  {
+    private static final Identifier ENDBRITE_ARMOR_TEXTURE = new Identifier("textures/endbrite_layer_1.png");
+    private final A endbriteArmorModel;
 
-    private final A endbriteHelmet;
-    private final A endbriteChestplate;
-
-    public CometArmorFeatureRenderer(FeatureRendererContext<T, M> featureRendererContext, A endbriteHelmet, A endbriteChestplate) {
+    public CometArmorFeatureRenderer(FeatureRendererContext<T, M> featureRendererContext, A endbriteArmorModel) {
         super(featureRendererContext);
-        this.endbriteHelmet = endbriteHelmet;
-        this.endbriteChestplate = endbriteChestplate;
+        this.endbriteArmorModel = endbriteArmorModel;
     }
 
+    @Override
     public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
-        ItemStack headItem = livingEntity.getEquippedStack(EquipmentSlot.HEAD);
-        ItemStack chestItem = livingEntity.getEquippedStack(EquipmentSlot.CHEST);
+        this.renderArmor(matrixStack, vertexConsumerProvider, livingEntity, EquipmentSlot.CHEST, i, endbriteArmorModel);
+        this.renderArmor(matrixStack, vertexConsumerProvider, livingEntity, EquipmentSlot.LEGS, i, endbriteArmorModel);
+        this.renderArmor(matrixStack, vertexConsumerProvider, livingEntity, EquipmentSlot.FEET, i, endbriteArmorModel);
+        this.renderArmor(matrixStack, vertexConsumerProvider, livingEntity, EquipmentSlot.HEAD, i, endbriteArmorModel);
+    }
 
-        // - Render helmet
-        if (headItem.getItem() == Comet.ENDBRITE_HELMET) {
-            matrixStack.push();
-            matrixStack.translate(0.0D, 0.0625D, 0.0D);
-            this.getContextModel().copyStateTo(this.endbriteHelmet);
-            this.endbriteHelmet.setAngles(livingEntity, f, g, j, k, l);
-            VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(ENDBRITE_HELMET_TEXTURE), false, headItem.hasGlint());
-            this.endbriteHelmet.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
-            matrixStack.pop();
+    private void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model) {
+        ItemStack itemStack = ((LivingEntity)entity).getEquippedStack(armorSlot);
+        if (!(itemStack.getItem() instanceof ArmorItem)) {
+            return;
         }
+        ArmorItem armorItem = (ArmorItem)itemStack.getItem();
+        if (armorItem.getSlotType() != armorSlot) {
+            return;
+        }
+        ((BipedEntityModel)this.getContextModel()).setAttributes(model);
+        this.setVisible(model, armorSlot);
+        boolean bl2 = itemStack.hasGlint();
+        if (armorItem instanceof DyeableArmorItem) {
+            int i = ((DyeableArmorItem)armorItem).getColor(itemStack);
+            float f = (float)(i >> 16 & 0xFF) / 255.0f;
+            float g = (float)(i >> 8 & 0xFF) / 255.0f;
+            float h = (float)(i & 0xFF) / 255.0f;
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, bl2, model, f, g, h, null);
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, bl2, model, 1.0f, 1.0f, 1.0f, "overlay");
+        } else {
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, bl2, model, 1.0f, 1.0f, 1.0f, null);
+        }
+    }
 
-        // - Render chestplate
-        if (chestItem.getItem() == Comet.ENDBRITE_CHESTPLATE){
-            matrixStack.push();
-            matrixStack.translate(0.0D, 0.0D, 0.0D);
-            this.getContextModel().copyStateTo(this.endbriteChestplate);
-            this.endbriteChestplate.setAngles(livingEntity, f, g, j, k, l);
-            VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(ENDBRITE_CHESTPLATE_TEXTURE), false, headItem.hasGlint());
-            this.endbriteChestplate.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
-            matrixStack.pop();
+    protected void setVisible(A bipedModel, EquipmentSlot slot) {
+        ((BipedEntityModel)bipedModel).setVisible(false);
+        switch (slot) {
+            case HEAD: {
+                ((BipedEntityModel)bipedModel).head.visible = true;
+                ((BipedEntityModel)bipedModel).hat.visible = true;
+                break;
+            }
+            case CHEST: {
+                ((BipedEntityModel)bipedModel).body.visible = true;
+                ((BipedEntityModel)bipedModel).rightArm.visible = true;
+                ((BipedEntityModel)bipedModel).leftArm.visible = true;
+                break;
+            }
+            case LEGS: {
+                ((BipedEntityModel)bipedModel).body.visible = true;
+                ((BipedEntityModel)bipedModel).rightLeg.visible = true;
+                ((BipedEntityModel)bipedModel).leftLeg.visible = true;
+                break;
+            }
+            case FEET: {
+                ((BipedEntityModel)bipedModel).rightLeg.visible = true;
+                ((BipedEntityModel)bipedModel).leftLeg.visible = true;
+            }
         }
+    }
+
+    private void renderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item, boolean glint, A model, float red, float green, float blue, @Nullable String overlay) {
+        VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumers, RenderLayer.getArmorCutoutNoCull(ENDBRITE_ARMOR_TEXTURE), false, glint);
+        ((AnimalModel)model).render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, red, green, blue, 1.0f);
     }
 }
