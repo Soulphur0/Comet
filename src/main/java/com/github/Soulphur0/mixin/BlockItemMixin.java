416634f2c04dbd27501401de343f4d073811d1dd
Soulphur <ismaelbe2002@gmail.com>
2022-12-05 20:33:40 +0100

    Made crystallized players unable to place blocks.

---
+++ src/main/java/com/github/Soulphur0/mixin/BlockItemMixin.java
@@ -0,0 +1,24 @@
+package com.github.Soulphur0.mixin;
+
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockItem.class)
+public class BlockItemMixin {
+
+    // ? Make crystallized players unable to place blocks.
+    @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+    private void cancelPlacement(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> cir){
+        if (context.getPlayer() != null){
+            if (context.getPlayer().isCrystallized()){
+                cir.setReturnValue(ActionResult.FAIL);
+            }
+        }
+
+    }
+}
