7e198ca6725ffa775b11f8c18d334f1dfa8c18a0
Soulphur <ismaelbe2002@gmail.com>
2022-12-26 02:27:45 +0100

    Made lava drenchstone break upon landing, with a growing probability the higher the lander has fallen; only falls greater than 1.25 blocks will throw a roll at breaking it. Made both water and lava drenchstone able to pick up with middle click; it will return regular drenchstone.

--- src/main/java/com/github/Soulphur0/dimensionalAlloys/block/LavaDrenchstoneBlock.java
+++ src/main/java/com/github/Soulphur0/dimensionalAlloys/block/LavaDrenchstoneBlock.java
@@ -1,11 +1,44 @@
 package com.github.Soulphur0.dimensionalAlloys.block;
 
+import com.github.Soulphur0.registries.CometBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class LavaDrenchstoneBlock extends DrenchstoneBlock{
 
     public LavaDrenchstoneBlock(Settings settings) {
         super(settings);
         setDefaultState(getStateManager().getDefaultState().with(LEVEL, 0).with(SOURCE_DIRECTION, Direction.UP));
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        if (!world.isClient()){
+            Random random = new Random();
+            float breakChance = (float)Math.floor(random.nextFloat()*10);
+            if (fallDistance > 1.25f){
+                if (breakChance-fallDistance <= 0){
+                    world.setBlockState(pos, Blocks.LAVA.getDefaultState().with(FluidBlock.LEVEL, 1));
+                    world.playSound(null, pos, SoundEvents.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, SoundCategory.BLOCKS,0.5f,0.2f);
+                }
+            }
+        }
+        super.onLandedUpon(world, state, pos, entity, fallDistance);
+    }
+
+    @Override
+    public ItemStack getPickStack(BlockView world, BlockPos pos, BlockState state) {
+        return new ItemStack(CometBlocks.DRENCHSTONE);
     }
 }
--- src/main/java/com/github/Soulphur0/dimensionalAlloys/block/WaterDrenchstoneBlock.java
+++ src/main/java/com/github/Soulphur0/dimensionalAlloys/block/WaterDrenchstoneBlock.java
@@ -1,12 +1,22 @@
 package com.github.Soulphur0.dimensionalAlloys.block;
 
+import com.github.Soulphur0.registries.CometBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 
 public class WaterDrenchstoneBlock extends DrenchstoneBlock{
 
     public WaterDrenchstoneBlock(Settings settings) {
         super(settings);
         setDefaultState(getStateManager().getDefaultState().with(LEVEL, 0).with(SOURCE_DIRECTION, Direction.UP));
+    }
+
+    @Override
+    public ItemStack getPickStack(BlockView world, BlockPos pos, BlockState state) {
+        return new ItemStack(CometBlocks.DRENCHSTONE);
     }
 }
 
